c48dc7d956ec992defbd13a15849cb94c8997c4d
Jiyeon970117 <80682733+Jiyeon970117@users.noreply.github.com>
2021-10-19 09:53:42 +0900

    v-3

--- js/about.js
+++ js/about.js
@@ -1,18 +1,24 @@
+const section = document.querySelector(".me")
 const text = document.querySelector(".text p")
 const content = document.querySelector(".content")
 const Skills = document.querySelectorAll(".skills-box")
 const arrowUP = document.querySelectorAll('.arrow-up')
-console.log(content.offsetTop)
+console.log(content)
+let innerWidth = window.innerWidth;
+
 // rotate 이벤트
 text.innerHTML = text.innerText.split("").map(
   (char, i) => `<span style="transform:rotate(${i * 13.5}deg)">${char}</span>`
 ).join("")
 
 // 스크롤 이벤트
+
 window.addEventListener('scroll', function(){
-  if(window.scrollY > 300){
-    content.classList.add("visible")
-  }
+    if(window.scrollY > 300){
+      content.classList.add("visible")
+    }  
+  // if(window.scrollY + window.innerHeight >= document.body.offsetHeight){
+  // }
 })
 
 for(let i = 0; i<arrowUP.length; i++){
@@ -21,3 +27,4 @@ for(let i = 0; i<arrowUP.length; i++){
     Skills[i].classList.toggle('active')
   })
 }
+
